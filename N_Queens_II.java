b8b3d5f5435d824a2cd5e5daa73d34c3f3bf6d19
Girish Vishnu Gawai <111906891+girishgawai@users.noreply.github.com>
2022-10-13 17:17:12 +0530

    Add files via upload

---
+++ N_Queens_II.java
@@ -0,0 +1,92 @@
+package leetcode.coomplex.code;
+
+import java.util.Scanner;
+
+public class N_Queens_II {
+	static int noOfWays=0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+//		int n = 4;
+		int n = sc.nextInt();
+		noOfWays=0;								// agr ye nhi kia toh test case fail hogi input 1 ke liye
+		// bcaz backend se  other many test case ayegi and hmne yha pe static kia hai ans list ko
+// toh purana solution v exists rhega usme isliye ek bar aur new krdo toh phle ka sab erase ho jayega
+
+		
+		boolean[][] board = new boolean[n][n];
+		fixDQueen(board,0,n);
+		System.out.println(noOfWays);
+	}
+
+	private static void fixDQueen(boolean[][] board, int row, int tnq) {
+		// TODO Auto-generated method stub
+		if(tnq==0)
+		{
+//			display(board);
+//			System.out.println();
+			noOfWays++;
+			return;
+		}
+		
+		for (int col = 0; col < board.length; col++) 
+		{
+			if(areQueensSafe(board,row,col)==true)
+			{
+				board[row][col]=true;
+				fixDQueen(board, row+1, tnq-1);
+				board[row][col]=false;
+			}
+			
+		}
+		
+	}
+
+	private static boolean areQueensSafe(boolean[][] board, int row, int col) {
+		// TODO Auto-generated method stub
+		// upper side
+		int r=row;
+		while(r>=0)
+		{
+			if(board[r][col]==true)
+				return false;
+			r--;
+		}
+		
+		// right upper side diagonal
+		r=row;
+		int c=col;
+		while(r>=0 && c<board.length)
+		{
+			if(board[r][c]==true)
+				return false;
+			r--;
+			c++;
+		}
+		
+		// left upper side diagonal
+		r=row;
+		c=col;
+		while(r>=0 && c>=0)
+		{
+			if(board[r][c]==true)
+				return false;
+			r--;
+			c--;
+		}
+		
+		return true;
+	}
+	
+	private static void display(boolean[][] board) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				System.out.print(board[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
