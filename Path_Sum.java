e29d3cb23f61be81dee1f9dc573b85749a08e957
Girish Gawai <111906891+girishgawai@users.noreply.github.com>
2023-07-02 14:31:17 +0530

    Tree Qps

--- Path_Sum.java
+++ Path_Sum.java
@@ -1,7 +1,7 @@
-package tree.qps;
+package trees;
 
 public class Path_Sum {
-	 
+	
 //	  Definition for a binary tree node.
 	  public class TreeNode {
 	      int val;
@@ -17,20 +17,18 @@ public class Path_Sum {
 	  }
 	 
 	class Solution {
-	    public boolean hasPathSum(TreeNode root, int targetSum) {
+	    public boolean hasPathSum(TreeNode root, int targetSum) 
+	    {
 	    	if(root==null)
 	    		return false;
 	    	
 	    	if(root.left==null && root.right==null)
 	    		return targetSum-root.val==0;
+
+	    	boolean lftsum = hasPathSum(root.left, targetSum-root.val);
+	    	boolean rtsum = hasPathSum(root.right, targetSum-root.val);
 	    	
-	    	
-	    	
-	    	boolean fs = hasPathSum(root.left, targetSum-root.val);
-	    	boolean ss = hasPathSum(root.right, targetSum-root.val);
-	    	
-//	    	return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
-	    	return fs || ss;
+	    	return lftsum || rtsum;
 	    }
 	}
 }
